0174bbc1ee6b4152b673fdf9e233f44396d2fe40
Julian Wong <julianwong2000@gmail.com>
2025-03-09 22:09:44 +0000

    Removed HF API key as an environment variable

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,3 +1,1 @@
-// Get your API key from https://huggingface.co/settings/tokens
-export const HF_API_KEY = process.env.EXPO_PUBLIC_HF_API_KEY ?? '';
 export const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_URL ?? '';
